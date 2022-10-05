e62c0b6f1f508c89cfef6ff185fa2dd68ebbec35
Naomi Carrigan <nhcarrigan@gmail.com>
2022-10-04 22:53:46 -0700

    feat: update games order, add new games, add playtime estimates (#15)
    * feat: new games + reorder
    
    * feat: add playtime estimates

--- src/assets/data/games.ts
+++ src/assets/data/games.ts
@@ -4,61 +4,176 @@ export const games: Game[] = [
   {
     name: 'Noel: The Mortal Fate',
     url: 'https://store.steampowered.com/app/850000/Noel_the_Mortal_Fate_S17/',
-    cover:
-      'https://cdn.akamai.steamstatic.com/steam/apps/850000/header.jpg?t=1628583956',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/850000/header.jpg',
+    playtime: 12,
   },
   {
     name: 'Nina Aquila: Legal Eagle',
     url: 'https://store.steampowered.com/app/1547290/Nina_Aquila_Legal_Eagle_Season_One/',
-    cover:
-      'https://cdn.akamai.steamstatic.com/steam/apps/1547290/header.jpg?t=1659466757',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/1547290/header.jpg',
+    playtime: 8,
   },
   {
     name: 'Here Comes Niko!',
     url: 'https://store.steampowered.com/app/925950/Here_Comes_Niko/',
-    cover:
-      'https://cdn.akamai.steamstatic.com/steam/apps/925950/header.jpg?t=1628006411',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/925950/header.jpg',
+    playtime: 6,
   },
   {
     name: 'Tuneria',
     url: 'https://store.steampowered.com/app/1268700/Tuneria/',
-    cover:
-      'https://cdn.akamai.steamstatic.com/steam/apps/1268700/header.jpg?t=1603500122',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/1268700/header.jpg',
+    playtime: 8,
   },
   {
     name: 'Danganronpa: Trigger Happy Havoc',
     url: 'https://store.steampowered.com/app/413410/Danganronpa_Trigger_Happy_Havoc/',
-    cover:
-      'https://cdn.akamai.steamstatic.com/steam/apps/413410/header.jpg?t=1637303326',
-  },
-  {
-    name: "Ocean's Heart",
-    url: 'https://store.steampowered.com/app/1393750/Oceans_Heart/',
-    cover:
-      'https://cdn.akamai.steamstatic.com/steam/apps/1393750/header.jpg?t=1647845967',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/413410/header.jpg',
+    playtime: 31,
   },
   {
     name: 'Danganronpa 2: Goodbye Despair',
     url: 'https://store.steampowered.com/app/413420/Danganronpa_2_Goodbye_Despair/',
-    cover:
-      'https://cdn.akamai.steamstatic.com/steam/apps/413420/header.jpg?t=1645515459',
-  },
-  {
-    name: 'Garden Story',
-    url: 'https://store.steampowered.com/app/1062140/Garden_Story/',
-    cover:
-      'https://cdn.akamai.steamstatic.com/steam/apps/1062140/header.jpg?t=1657642005',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/413420/header.jpg',
+    playtime: 41,
   },
   {
     name: 'Danganronpa V3: Killing Harmony',
     url: 'https://store.steampowered.com/app/567640/Danganronpa_V3_Killing_Harmony/',
-    cover:
-      'https://cdn.akamai.steamstatic.com/steam/apps/567640/header.jpg?t=1654081483',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/567640/header.jpg',
+    playtime: 52,
   },
+  {
+    name: "Ocean's Heart",
+    url: 'https://store.steampowered.com/app/1393750/Oceans_Heart/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/1393750/header.jpg',
+    playtime: 11,
+  },
+  {
+    name: 'Garden Story',
+    url: 'https://store.steampowered.com/app/1062140/Garden_Story/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/1062140/header.jpg',
+    playtime: 16,
+  },
+
   {
     name: 'Soaring Machinariae',
     url: 'https://store.steampowered.com/app/1616030/Soaring_Machinariae/',
-    cover:
-      'https://cdn.akamai.steamstatic.com/steam/apps/1616030/header.jpg?t=1654506116',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/1616030/header.jpg',
+    playtime: 12,
+  },
+  {
+    name: 'Omori',
+    url: 'https://store.steampowered.com/app/1150690/OMORI/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/1150690/header.jpg',
+    playtime: 28,
+  },
+  {
+    name: 'Ikenfell',
+    url: 'https://store.steampowered.com/app/854940/Ikenfell/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/854940/header.jpg',
+    playtime: 21,
+  },
+  {
+    name: 'To The Moon',
+    url: 'https://store.steampowered.com/app/206440/To_the_Moon/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/206440/header.jpg',
+    playtime: 5,
+  },
+  {
+    name: 'A Bird Story',
+    url: 'https://store.steampowered.com/app/327410/A_Bird_Story/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/327410/header.jpg',
+    playtime: 2,
+  },
+  {
+    name: 'Finding Paradise',
+    url: 'https://store.steampowered.com/app/337340/Finding_Paradise/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/337340/header.jpg',
+    playtime: 6,
+  },
+  {
+    name: 'Impostor Factory',
+    url: 'https://store.steampowered.com/app/1182620/Impostor_Factory/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/1182620/header.jpg',
+    playtime: 4,
+  },
+  {
+    name: "Queen's Wish: The Conqueror",
+    url: 'https://store.steampowered.com/app/1058130/Queens_Wish_The_Conqueror/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/1058130/header.jpg',
+    playtime: 52,
+  },
+  {
+    name: "Queen's Wish 2: The Tormentor",
+    url: 'https://store.steampowered.com/app/1947750/Queens_Wish_2_The_Tormentor/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/1947750/header.jpg',
+    playtime: 52,
+  },
+  {
+    name: 'Anodyne',
+    url: 'https://store.steampowered.com/app/234900/Anodyne/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/234900/header.jpg',
+    playtime: 8,
+  },
+  {
+    name: 'Shalnor Legends: Sacred Lands',
+    url: 'https://store.steampowered.com/app/651680/Shalnor_Legends_Sacred_Lands/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/651680/header.jpg',
+    playtime: 9,
+  },
+  {
+    name: 'Avernum: Escape From The Pit',
+    url: 'https://store.steampowered.com/app/208400/Avernum_Escape_From_the_Pit/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/208400/header.jpg',
+    playtime: 60,
+  },
+  {
+    name: 'Avernum 2: Crystal Souls',
+    url: 'https://store.steampowered.com/app/337850/Avernum_2_Crystal_Souls',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/337850/header.jpg',
+    playtime: 79,
+  },
+  {
+    name: 'Avernum 3: Ruined World',
+    url: 'https://store.steampowered.com/app/691830/Avernum_3_Ruined_World',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/691830/header.jpg',
+    playtime: 72,
+  },
+  {
+    name: 'Avernum 4',
+    url: 'https://store.steampowered.com/app/206020/Avernum_4',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/206020/header.jpg',
+    playtime: 64,
+  },
+  {
+    name: 'Avernum 5',
+    url: 'https://store.steampowered.com/app/206040/Avernum_5/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/206040/header.jpg',
+    playtime: 61,
+  },
+  {
+    name: 'Avernum 6',
+    url: 'https://store.steampowered.com/app/206060/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/206060/header.jpg',
+    playtime: 64,
+  },
+  {
+    name: 'Avadaon: The Black Fortress',
+    url: 'https://store.steampowered.com/app/112100/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/112100/header.jpg',
+    playtime: 54,
+  },
+  {
+    name: 'Avadon 2: The Corruption',
+    url: 'https://store.steampowered.com/app/233310',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/233310/header.jpg',
+    playtime: 46,
+  },
+  {
+    name: 'Avadon 3: The Warborn',
+    url: 'https://store.steampowered.com/app/460780',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/460780/header.jpg',
+    playtime: 47,
   },
 ];
