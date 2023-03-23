d8fcae6e5d5cabd0a1780a770ee06d1640efa92c
Naomi Carrigan <nhcarrigan@gmail.com>
2023-03-22 17:02:16 -0700

    feat: stream queue -> game queue

--- src/assets/data/games.ts
+++ src/assets/data/games.ts
@@ -2,31 +2,40 @@ import { Game } from '../interfaces/Game';
 
 export const games: Game[] = [
   {
+    name: 'Chrono Cross',
+    url: 'https://store.steampowered.com/app/1133760/CHRONO_CROSS_THE_RADICAL_DREAMERS_EDITION/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/1133760/header.jpg',
+    playtime: 42,
+  },
+  {
     name: 'Octopath Traveler 2',
     url: 'https://store.steampowered.com/app/1971650/OCTOPATH_TRAVELER_II/',
     cover:
       'https://cdn.cloudflare.steamstatic.com/steam/apps/1971650/header.jpg',
-    playtime: 70,
+    playtime: 80,
+  },
+  {
+    name: 'Knight Bewitched',
+    url: 'https://store.steampowered.com/app/815150/Knight_Bewitched/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/815150/header.jpg',
+    playtime: 13,
+  },
+  {
+    name: 'Knight Bewitched 2',
+    url: 'https://store.steampowered.com/app/1308250/Knight_Bewitched_2/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/1308250/header.jpg',
+    playtime: 0,
+  },
+  {
+    name: 'Celestial Hearts',
+    url: 'https://store.steampowered.com/app/1434350/Celestial_Hearts/',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/1434350/header.jpg',
+    playtime: 0,
   },
   {
     name: 'Absinthia',
     url: 'https://store.steampowered.com/app/1848070/Absinthia/',
-    cover:
-      'https://cdn.cloudflare.steamstatic.com/steam/apps/1848070/header.jpg',
+    cover: 'https://cdn.akamai.steamstatic.com/steam/apps/1848070/header.jpg',
     playtime: 0,
-  },
-  {
-    name: 'Wo Long: Fallen Dynasty',
-    url: 'https://store.steampowered.com/app/1448440/Wo_Long_Fallen_Dynasty/',
-    cover:
-      'https://cdn.cloudflare.steamstatic.com/steam/apps/1448440/header.jpg?t=1677836173',
-    playtime: 0,
-  },
-  {
-    name: 'Toukiden 2',
-    url: 'https://store.steampowered.com/app/551730/Toukiden_2/',
-    cover:
-      'https://cdn.cloudflare.steamstatic.com/steam/apps/551730/header.jpg',
-    playtime: 32,
   },
 ];
